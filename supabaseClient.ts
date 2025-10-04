5ec5b6ccc0b06ce723704128a17f0106d7cbb70d
stellaqueen15 <brendamisenga@gmail.com>
2025-10-04 18:56:22 -0400

    second commit

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const supabaseUrl = "https://pqxeccwmswhqyawxswfu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxeGVjY3dtc3docXlhd3hzd2Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMzQxMzcsImV4cCI6MjA3MzgxMDEzN30.k82G59z3ELFo1cO-3xPhPaKJA_8aK2tS9hapsG4uSRQ";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
